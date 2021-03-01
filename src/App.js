5da59eca82ad116d2edc17d0ca12b09d62b6253d
pachuley <fjsabate@gmail.com>
2021-03-01 15:20:26 -0300

    App renders, color scheme chosen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     return (
       <>
         <h1>Hello</h1>
+        <p>Bacon ipsum dolor amet tri-tip ribeye pork chop swine. Turducken pastrami burgdoggen ham hock, strip steak flank short loin doner ground round andouille corned beef fatback kevin chuck leberkas. Short loin cow flank, tri-tip pancetta jowl capicola chuck alcatra strip steak beef doner. Shoulder landjaeger alcatra sausage capicola short loin leberkas pig ribeye bacon chuck rump spare ribs prosciutto boudin.</p>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from './App';
+import './index.css';
 
 if (module.hot) {
     module.hot.accept()
 }
 
-var rootElement = document.getElementById("root")
-
-ReactDOM.render(
-    <App />, rootElement
-)
+ReactDOM.render(<App />, document.getElementById("root"))
