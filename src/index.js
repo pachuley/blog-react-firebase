655664ebb773096e9112f05eb4d84edb5cf59bad
pachuley <fjsabate@gmail.com>
2021-03-01 13:02:19 -0300

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from './App';
+
+if (module.hot) {
+    module.hot.accept()
+}
+
+var rootElement = document.getElementById("root")
+
+ReactDOM.render(
+    <App />, rootElement
+)
